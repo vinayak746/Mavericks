1e741057f45d3a88fb5234da86cb57efa4b2b222
Vinayak Arora <vinayakarora7461@gmail.com>
2025-01-10 11:46:25 +0530

    fix import paths for ShopContext and update Vite configuration for Mavericks deployment

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
-import { ShopContext } from '../context/shopContext'
+import { ShopContext } from '../context/ShopContext'
 
 
 const Navbar = () => {
