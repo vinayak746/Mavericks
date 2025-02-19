ddbb5c381486ef7acb507487cd0f8fa82ea43018
Vinayak Arora <vinayakarora7461@gmail.com>
2025-02-19 11:28:54 +0530

    server with cloudinary and mongodb

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+// App config
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+connectDB()
+connectCloudinary()
+
+// middlewares
+
+app.use(express.json());
+app.use(cors())
+
+// api endpoints
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(PORT,()=>console.log("Server Started on Port :" + PORT))
