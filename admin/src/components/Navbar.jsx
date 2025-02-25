de42c836eef342eaf88398745ee38db401dfdffa
Vinayak Arora <vinayakarora7461@gmail.com>
2025-02-25 11:22:38 +0530

    admin with twcss

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import logo from '../assets/logo.png'; // Correct import statement
 const Navbar = () => {
   return (
     <div>
-      <img className='w-full justify-center' src={logo} alt='Logo' />
-      <button>Logout</button>
+      <img className='w-[max(10%, 80px)] ' src={logo} alt='Logo' />
+      <button className='bg-gray-600 text-white py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm  '>Logout</button>
     </div>
   );
 };
--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port:5174}
+  plugins: [react(), tailwindcss()],
 })
